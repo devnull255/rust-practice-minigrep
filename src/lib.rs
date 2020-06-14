7a9391d768ef3269f5c4cf645392f4c03751e6cd
Michael Mabin <mabinm@comcast.net>
2020-06-14 15:52:23 -0500

    Add processing for CASE_INSENSITIVE env var

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::fs;
 use std::error::Error;
 
@@ -15,8 +16,9 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, case_sensitive })
     }
 }
     
